dac0aba88440575c3725ad61cc3d8384f2eeba29
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-07-30 15:16:37 -0500

    intro to dictionaries

---
+++ meal_dictionary.py
@@ -0,0 +1,17 @@
+meal = {
+    "Drink":"Coke",
+    "Appetizer":"Chips",
+    "Entree":"Tacos",
+    "Dessert":"Churros"
+}
+
+print('This Thursday I will have %s and %s for dinner' % (meal['Drink'],meal["Entree"]))
+
+if "Dessert" in meal:
+    print('Of Course I will have Dessert')
+    meal["Dessert"]="ice-cream"
+    print(meal)
+else:
+    print('Need to find some dessert')
+    # meal['Dessert'] = "Churros"
+    # print(meal)
