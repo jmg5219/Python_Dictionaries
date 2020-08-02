5dc1bc53592145b338527a122038776924c5e09f
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-08-02 14:09:45 -0500

    Program to investigate looping in Dictionaries

---
+++ Iterate_nested_dicts.py
@@ -0,0 +1,20 @@
+D = {'emp1': {'name': 'Bob', 'job': 'Mgr'},
+     'emp2': {'name': 'Kim', 'job': 'Dev'},
+     'emp3': {'name': 'Sam', 'job': 'Dev'}}
+
+for id, info in D.items():
+    print("\nEmployee ID:", id)
+    for key in info: #for every value print the key
+        print(key + ':', info[key])
+
+# Prints Employee ID: emp1
+#        name: Bob
+#        job: Mgr
+
+#        Employee ID: emp2
+#        name: Kim
+#        job: Dev
+
+#        Employee ID: emp3
+#        name: Sam
+#        job: Dev
